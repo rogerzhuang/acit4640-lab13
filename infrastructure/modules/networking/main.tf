6f7ac575df65c3c02c3576b8aa6f8cd23516b028
rogerzhuang <87434722+rogerzhuang@users.noreply.github.com>
2024-12-03 09:33:38 -0800

    updated route table

--- infrastructure/modules/networking/main.tf
+++ infrastructure/modules/networking/main.tf
@@ -38,7 +38,7 @@ resource "aws_subnet" "private" {
   }
 }
 
-resource "aws_route_table" "public" {
+resource "aws_route_table" "main" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -47,31 +47,18 @@ resource "aws_route_table" "public" {
   }
 
   tags = {
-    Name = "${var.project_name}-public-rt"
-  }
-}
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  tags = {
-    Name = "${var.project_name}-private-rt"
+    Name = "${var.project_name}-main-rt"
   }
 }
 
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
+  route_table_id = aws_route_table.main.id
 }
 
 resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.main.id
 }
 
 # Security Groups
